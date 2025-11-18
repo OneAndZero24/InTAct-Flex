158aefe5810f593678cf181ec4cb09a5fe136b9d
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-11-19 00:04:56 +0100

    Update the best hyperparameters

---
+++ scripts/optimal_hyperparams/dil/mnist/intact/intact.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=split_mnist_mlp_intact_optimal_hyperparams
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/optimal_hyperparams/dil/mnist/intact/intact"
