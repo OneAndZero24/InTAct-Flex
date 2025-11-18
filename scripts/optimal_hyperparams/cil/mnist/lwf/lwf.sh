2c1401104fa0a199b7b1883df9136a3a04421450
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-11-18 15:16:29 +0100

    Update scripts

---
+++ scripts/optimal_hyperparams/cil/mnist/lwf/lwf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=split_mnist_mlp_lwf_cil
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/optimal_hyperparams/cil/mnist/lwf/lwf"
---
+++ scripts/optimal_hyperparams/dil/mnist/ewc/ewc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=split_mnist_mlp_ewc_dil
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/optimal_hyperparams/dil/mnist/ewc/ewc"
+
